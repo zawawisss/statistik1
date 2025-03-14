fb0341510065714447352cf426718a036866ef6b
Zawawi Sayyid Shihab <awi@MacBook-Pro-Zawawi.local>
2025-03-15 02:06:18 +0700

    2

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone', // Outputs a Single-Page Application (SPA).
-    distDir: './dist', // Changes the build output directory to `./dist/`.
-    reactStrictMode: true,
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  }
-   
-  export default nextConfig
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Pastikan ini ada untuk App Router
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
